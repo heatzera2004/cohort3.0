d6aa321f7022e69ce41646280254c54cdd31c7c8
heatzera2004 <yousuf04mar@gmail.com>
2024-08-11 23:00:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// class Rectangle{
+//     constructor(width , height , color){
+//       this.width = width;
+//       this.height = height;
+//       this.color = color;
+//     }
+  
+//     area(){
+//       const area = this.width * this.height;
+//       return area;
+//     }
+  
+//     paint(){
+//       console.log(`Painting with color : ${this.color}`)
+//     }
+    
+//   }
+  
+//   const rect = new Rectangle(2 , 4 , "red");
+//   const rect2 = new Rectangle(10 , 20 , "black")
+//   const area = rect.area()
+//   rect.paint()
+//   console.log(rect2.area());
+//   rect2.paint()
+
+// const date = new Date()
+// console.log(date.getFullYear())
